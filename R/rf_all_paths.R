d3c3add6f2f5d74bc7d2ae1fe2be58994254d74e
Ben R Fitzpatrick <ben.r.fitzpatrick@gmail.com>
2018-12-09 17:10:44 +0100

    added a new argument, parallel, that gives the option to replace the for loop that runs tree_paths for each successive tree in the forest in serial with foreach::%dopar% which divides the trees of the forest between the nodes of a user created cluster and executes tree_paths in parallel on this cluster

--- R/rf_all_paths.R
+++ R/rf_all_paths.R
@@ -3,23 +3,21 @@
 #' \code{rf_all_paths} is a low level function called by \code{\link{rf_pathfinder}} to determine all paths through all trees in a random forest.
 #'
 #' @param rf An object of class \code{randomForest} returned by \code{\link[randomForest]{randomForest}}
-#' @param report.progress if \code{TRUE} the percentage of trees for which all paths have been found is printed to the console as the function runs, if \code{FALSE} no such progress report is printed
+#' @param parallel Logical: should the paths through the trees of the random forest be explored using parallel computing? TRUE/FALSE. Note you will need to create and register a cluster before calling this function to have it search trees in parallel with this option. See the Examples.
 #' @export
-rf_all_paths <- function(rf, report.progress = TRUE){ 
+rf_all_paths <- function(rf, parallel = parallel){ 
     n.trees <- rf$ntree
-    paths.ls <- vector(mode = 'list', length = n.trees)
-    if(report.progress == TRUE){
-      for(i in 1:n.trees){
-        paths.ls[[i]] <- tree_paths(rf = rf, tree = i, plot.network = FALSE)
-        print(paste(round(100*i/n.trees, digits = 4), '% of forest explored'))
+    if(parallel == TRUE){
+      paths <- foreach::foreach(i = 1:n.trees, .combine = dplyr::bind_rows) %dopar% {
+      tree_paths(rf = rf, tree = i, plot.network = FALSE)
       }
-      print('combining dataframes')
-    } else{  
+    } else{
+        paths.ls <- vector(mode = 'list', length = n.trees)
         for(i in 1:n.trees){
-          paths.ls[[i]] <- rf_to_igraph(rf = rf, tree = i, plot.network = FALSE) 
+          paths.ls[[i]] <- tree_paths(rf = rf, tree = i, plot.network = FALSE)
         }
+        paths <- Reduce(f = dplyr::bind_rows, x = paths.ls) # would purrr::reduce be better here?
     }
-    paths <- Reduce(f = dplyr::bind_rows, x = paths.ls)
-    paths[is.na(paths)] <- 'Terminus'
+    paths[is.na(paths)] <- 'Terminus' 
     return(paths)
 }
